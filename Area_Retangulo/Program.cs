5e8891b8f9e662253422b70057f689b4c8795ee5
CrystoferAT <crystofer.tre@cs.up.edu.br>
2026-03-08 16:05:25 -0300

    add Area Retangulo

---
+++ Area_Retangulo/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Digite a altura do retangulo: ");
+double altura = double.Parse(Console.ReadLine());
+
+Console.Write("Digite a largura do retangulo: ");
+double largura = double.Parse(Console.ReadLine());
+
+double area = altura*largura;
+Console.Write($"A area do retangulo é {area}.");
