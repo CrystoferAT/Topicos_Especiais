725d8e9c6bf5f7d49593189a70afd288319fbad2
CrystoferAT <crystofer.tre@cs.up.edu.br>
2026-03-14 16:52:58 -0300

    Add exercicio tabuada

---
+++ Tabuada/Program.cs
@@ -0,0 +1,10 @@
+﻿
+Console.WriteLine("Tabuada");
+Console.Write("Digite um numero inteiro para gerar uma tabuada :");
+int num = int.Parse(Console.ReadLine());
+int i, resultado;
+
+for(i=1; i <= 10; i++)
+{
+    Console.WriteLine($"{i} x {num} =" + i*num);
+}
