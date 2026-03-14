333c354a6a8aad89a35ff4a49eca6df7a4d95add
CrystoferAT <crystofer.tre@cs.up.edu.br>
2026-03-14 17:42:53 -0300

    Add exercicio login

---
+++ Sistema_senha/Program.cs
@@ -0,0 +1,27 @@
+﻿
+Console.WriteLine("Login");
+Console.Write("Defina um nome de Usuario: ");
+string user = (Console.ReadLine());
+Console.Write("Defina uma senha: ");
+string key =(Console.ReadLine());
+bool log = false;
+
+
+    while(log == false)
+    {
+        Console.Write("Digite seu nome de Usuario: ");
+        string user2 = (Console.ReadLine());
+        Console.Write("Digite sua senha: ");
+        string key2 =(Console.ReadLine());
+            
+            if(user == user2 && key2 == key)
+            {
+                Console.WriteLine("Login efetuado com sucesso.");
+                Console.WriteLine($"Seja bem vindo(a) {user}");
+                log = true;
+            }else{
+                Console.WriteLine("Senha incorreta tente novamente.");
+            }
+    }
+
+Console.WriteLine($"Usuario: {user} e  senha: {key}");
