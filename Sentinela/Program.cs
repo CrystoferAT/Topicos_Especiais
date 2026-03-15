0df0740544d3e305eb5e098a9756a165fa3f874f
CrystoferAT <crystofer.tre@cs.up.edu.br>
2026-03-15 10:02:20 -0300

    Add exercicios sistema_senha

---
+++ Sentinela/Program.cs
@@ -0,0 +1,22 @@
+﻿
+int num, soma = 0,contador = 0, maior = int.MinValue;
+double media = 0;
+
+do{
+Console.WriteLine("Digite um numero inteiro ou digite zero para parar e exibir o resultado da soma total media e maior numero digitado:");
+    num = int.Parse(Console.ReadLine());
+    soma += num;
+    ++contador;
+    if(num > maior)
+    {
+        maior = num;
+    }
+}while(num != 0);
+
+if(contador > 0)
+{
+    media = (double)soma/(contador-1);
+    Console.WriteLine("A soma é: " +soma);
+    Console.WriteLine("A media é: " +media);
+    Console.WriteLine("O maior valor digitado foi : " +maior);
+}
--- Sistema_senha/Program.cs
+++ Sistema_senha/Program.cs
@@ -5,23 +5,30 @@ string user = (Console.ReadLine());
 Console.Write("Defina uma senha: ");
 string key =(Console.ReadLine());
 bool log = false;
+int tentativa = 0;
 
-
-    while(log == false)
+        while(log == false && tentativa < 3)
+        {
+            Console.Write("Digite seu nome de Usuario: ");
+            string user2 = (Console.ReadLine());
+            Console.Write("Digite sua senha: ");
+            string key2 =(Console.ReadLine());
+                
+                if(user == user2 && key2 == key)
+                {
+                    Console.WriteLine("Login efetuado com sucesso.");
+                    Console.WriteLine($"Seja bem vindo(a) {user}");
+                    log = true;
+                    Console.WriteLine($"Usuario: {user} e  senha: {key}");
+                    break;
+                }else{
+                    Console.WriteLine("Senha incorreta tente novamente.");
+                    tentativa++;
+                }
+        }
+        if(tentativa == 3)
     {
-        Console.Write("Digite seu nome de Usuario: ");
-        string user2 = (Console.ReadLine());
-        Console.Write("Digite sua senha: ");
-        string key2 =(Console.ReadLine());
-            
-            if(user == user2 && key2 == key)
-            {
-                Console.WriteLine("Login efetuado com sucesso.");
-                Console.WriteLine($"Seja bem vindo(a) {user}");
-                log = true;
-            }else{
-                Console.WriteLine("Senha incorreta tente novamente.");
-            }
+        Console.Write("Exesso de tentativas.");      
     }
+    
 
-Console.WriteLine($"Usuario: {user} e  senha: {key}");
