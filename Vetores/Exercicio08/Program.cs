59c8b5dfb217a55323c6fffc673cadeb671a7a14
CrystoferAT <crystofer.tre@cs.up.edu.br>
2026-03-21 07:36:12 -0300

    Finalizacao Exercicios

---
+++ Vetores/Exercicio08/Program.cs
@@ -0,0 +1,38 @@
+﻿
+using System.Globalization;
+
+int[] numeros = new int[12];
+int[] pares = new int[12];
+int[] impares = new int[12];
+
+int cont1 = 0;
+int cont2 = 0;
+
+for(int i = 0; i < numeros.Length; i++)
+{
+    Console.WriteLine($"Digite 12 numeros inteiros para alimentar o vetor: {i + 1}");
+    numeros[i] = int.Parse(Console.ReadLine());
+
+    if( numeros[i] % 2 == 0)
+    {
+        pares[cont1]= numeros[i];
+        cont1++;
+    }
+    else
+    {
+        impares[cont2]= numeros[i];
+        cont2++;
+    }
+}
+Console.WriteLine("Vetor de pares");
+for(int i = 0; i < cont1; i++)
+{
+    Console.Write($"{pares[i]} ");
+}
+
+Console.WriteLine("\nVetor de impares");
+for(int j = 0; j < cont2; j++)
+{
+    Console.Write($"{impares[j]} ");
+}
+
